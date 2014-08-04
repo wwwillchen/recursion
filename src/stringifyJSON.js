397f24935622c5a579f254942acc4009028d3ce3
Will Chen <willchen90@gmail.com>
2014-08-04 09:39:22 -0700

    mid-way in progress, stuck on array with strings inside elements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,44 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(input, arr) {
+	console.log("first input:");
+	console.log(input);
+	var stringInput, arrayInput;
+	if (Array.isArray(input)==false) {
+		if (input === null) {
+			input = "null";
+		} else if (typeof input === "string") {
+			input = '"' + input + '"';
+		}
+		stringInput = input.toString();
+		
+	} else {
+		stringInput = "[" + input.toString() + "]";
+	}
+	arrayInput = stringInput.split("");
+	
+	console.log("Input: " + arrayInput);
+	console.log(Array.isArray(arrayInput));
+	var arrayOutput = arr || [];
+	console.log(arrayOutput);
+	
+	for (var i = 0, l = arrayInput.length; i < l; i++) {
+		arrayOutput.push(arrayInput.shift());
+		console.log(typeof arrayOutput);
+	}
+	
+	/*
+
+	if(arrayInput.length > 0) {
+		console.log("Input length: " + arrayInput.length);
+		console.log("Input for recursion: " + arrayInput);
+		stringifyJSON(arrayInput,arrayOutput);
+	}
+	*/
+
+		console.log("break");
+		var finalOutput = arrayOutput.join("");
+		console.log("Final: " + finalOutput);
+		return finalOutput;		
 };
